62933d85aa424af93e6d0699934df93601038ac1
danielavila <daniaviladomingo@gmail.com>
2023-05-26 22:24:51 +0200

    remove unused oncreate method

--- app/src/main/java/com/cabify/challenge/CabifyTechnicalChallenge.kt
+++ app/src/main/java/com/cabify/challenge/CabifyTechnicalChallenge.kt
@@ -4,8 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class CabifyTechnicalChallenge : Application() {
-    override fun onCreate() {
-        super.onCreate()
-    }
-}
+class CabifyTechnicalChallenge : Application()
